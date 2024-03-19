9390877268d26c070ca7b7427e2a11d1f30c17c6
tanvirlawsikho <tanvir@lawsikho.in>
2024-03-20 01:39:49 +0600

    added menu section in homepage

--- src/app/public-module/home/home.component.ts
+++ src/app/public-module/home/home.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  public topItems: any[] = [
+    {
+      name: 'Special Burger',
+      price: 50,
+      details: 'Great combination of sauce and meyonese',
+    },
+    {
+      name: 'Pasta',
+      price: 50,
+      details: 'Great combination of sauce and meyonese',
+    },
+    {
+      name: 'Nachos',
+      price: 70,
+      details: 'Great combination of sauce and meyonese',
+    },
+    {
+      name: 'Chickent Munchurian',
+      price: 100,
+      details: 'Great combination of sauce and meyonese',
+    },
+  ];
 }
