a2845e27fb2c174482acff1c0a38de83daf24885
tanvirlawsikho <tanvir@lawsikho.in>
2024-03-29 04:38:08 +0600

    firebase auth implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,9 +10,32 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+const firebaseConfig = {
+  apiKey: 'AIzaSyBvHGJLNSULUWjwomFl5GQpPt2ff-TV2gk',
+  authDomain: 'street-admin.firebaseapp.com',
+  projectId: 'street-admin',
+  storageBucket: 'street-admin.appspot.com',
+  messagingSenderId: '466201171483',
+  appId: '1:466201171483:web:65b0d2cd16c9e5d0670abe',
+  measurementId: 'G-RG0XJY5MDR',
+};
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideNzIcons(), provideNzI18n(en_US), importProvidersFrom(FormsModule), importProvidersFrom(), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideNzIcons(),
+    provideNzI18n(en_US),
+    importProvidersFrom(FormsModule),
+
+    // importProvidersFrom(HttpClientModule),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(firebaseConfig)),
+      provideAuth(() => getAuth()),
+    ]),
+    provideAnimations(),
+  ],
 };
