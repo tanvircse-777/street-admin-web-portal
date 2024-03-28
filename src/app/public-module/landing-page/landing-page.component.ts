79f429ec566fa5118f2f2c71fb38fc5726c249b0
tanvirlawsikho <tanvir@lawsikho.in>
2024-03-29 02:19:36 +0600

    new changes on landing page

--- src/app/public-module/landing-page/landing-page.component.ts
+++ src/app/public-module/landing-page/landing-page.component.ts
@@ -18,6 +18,8 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   public currentIndex: number = 0;
   private intervalSubscription: Subscription = new Subscription();
   public backgroundImageUrls: string[] = [
+    // 'https://img.freepik.com/free-photo/top-view-fast-food-concept-with-copyspace_23-2147819594.jpg?t=st=1711049248~exp=1711052848~hmac=6f5d087cc95754ab51c494bdef970b8a9c2c1ee8a202057531bf9b1628730c1d&w=1380',
+    './../../../assets/images/homepage/backup/top-view-fried-egg-with-asparagus.jpg',
     './../../../assets/images/homepage/background_1.jpg',
     './../../../assets/images/homepage/background_2.jpg',
     './../../../assets/images/homepage/background_3.jpg',
@@ -57,11 +59,10 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   }
 
   updateBackgroundImage() {
-    // this.currentIndex =
-    //   (this.currentIndex + 1) % this.backgroundImageUrls.length;
     this.backgroundImageUrl = this.backgroundImageUrls[this.currentIndex];
     this.currentIndex++;
-    if (this.currentIndex == 3) this.currentIndex = 0;
+    if (this.currentIndex == this.backgroundImageUrls.length)
+      this.currentIndex = 0;
   }
 
   ngOnDestroy() {
