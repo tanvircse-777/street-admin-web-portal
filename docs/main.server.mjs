d4d3740737ea86288467785349890151066475ae
tanvirlawsikho <tanvir@lawsikho.in>
2024-05-15 23:59:21 +0600

    sprint for github

--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-P4GZAE66.mjs";import"./chunk-XCOFWXAR.mjs";import"./chunk-YBRLBQ4F.mjs";import"./chunk-3K3VNCHJ.mjs";import"./chunk-ZQQCJHUH.mjs";import"./chunk-3SZVCSKE.mjs";import"./chunk-H6KHSOBK.mjs";export{a as default};
