af99274f8805ddde915c65da420f9b59604a5d5e
tanvirlawsikho <tanvir@lawsikho.in>
2024-05-16 13:43:39 +0600

    some changes

--- src/app/public-module/landing-page/landing-page.component.ts
+++ src/app/public-module/landing-page/landing-page.component.ts
@@ -6,19 +6,12 @@ import {
   OnInit,
   PLATFORM_ID,
 } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Subscription } from 'rxjs';
 import {
   Feedback,
   FeedbackService,
 } from '../../shared/services/feedback.service';
-import { error } from 'console';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
@@ -58,6 +51,7 @@ export class LandingPageComponent implements OnInit, OnDestroy {
       details: 'Great combination of sauce and meyonese',
     },
   ];
+  public feedbackList: Feedback[] = [];
 
   public feedbackForm: FormGroup = new FormGroup({
     name: new FormControl(),
@@ -77,7 +71,6 @@ export class LandingPageComponent implements OnInit, OnDestroy {
       // this.intervalSubscription = interval(5000).subscribe(() => {
       //   this.updateBackgroundImage();
       // });
-      this.setFeedbackForm();
       this.getAllFeedback();
     }
   }
@@ -89,50 +82,26 @@ export class LandingPageComponent implements OnInit, OnDestroy {
       this.currentIndex = 0;
   }
 
-  setFeedbackForm() {
-    
-    // this.feedbackForm = this._fb.group({
-    //   name: [undefined, Validators.compose([])],
-    //   email: [undefined, Validators.compose([])],
-    //   feedback: [undefined, Validators.compose([])],
-    // });
-    // this.feedbackForm = new FormGroup({
-
-    // });
-  }
-
-  public feedbackList: Feedback[] = [];
   getAllFeedback() {
-    
     this.feedbackService
       .getAllFeedback()
       .snapshotChanges()
       .subscribe({
         next: (data: any) => {
-          
-          // this.feedbackList = data[0].payload.val() as {
-          //   id: number;
-          //   name: string;
-          //   email: string;
-          // };
           this.feedbackList = [];
           data.map((action: any) => {
-            const data = action.payload.val(); // Assuming Feedback interface is defined
-            // const key = action.payload.key; // If you need the Firebase key
-            this.feedbackList.push(data); // Add Firebase key to the object if needed
+            const data = action.payload.val();
+
+            this.feedbackList.push(data);
           });
-          console.log('feedback list');
-          console.log(this.feedbackList);
         },
         error: (error: any) => {
-          
           console.log(error);
         },
       });
   }
 
   addFeedback() {
-    
     this.feedbackService.addFeedback(this.feedbackForm.value as Feedback);
   }
   ngOnDestroy() {
