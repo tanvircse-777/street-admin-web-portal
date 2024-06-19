e178ff980f012f57079f3bbab69373e6ca37aaeb
tanvirlawsikho <tanvir@lawsikho.in>
2024-06-20 00:09:46 +0600

    new deployment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: false,
+  // baseForntendURL: 'http://localhost:4200',
+  // baseApiUrl: 'http://localhost:3001/',
+  // returnUrl: 'http://localhost:4200/',
   baseForntendURL: 'http://localhost:4200',
-  baseApiUrl: 'http://localhost:3001/',
+  baseApiUrl: 'https://street-admin-api-portal.onrender.com/',
   returnUrl: 'http://localhost:4200/',
 };
