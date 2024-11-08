244d8d32dc32999363438d3ad4550d51a584f157
tanvirlawsikho <tanvir@lawsikho.in>
2024-11-08 23:08:41 +0600

    on progress

--- src/app/authenticated-module/admin/admin-routing.module.ts
+++ src/app/authenticated-module/admin/admin-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { AddMonthlySellCostComponent } from './components/add-monthly-sell-cost/add-monthly-sell-cost.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
-    children: [{ path: 'dashboard', component: AdminDashboardComponent }],
+    children: [
+      { path: 'dashboard', component: AdminDashboardComponent },
+
+      { path: 'add-monthly-sell-cost', component: AddMonthlySellCostComponent },
+    ],
   },
 ];
 
